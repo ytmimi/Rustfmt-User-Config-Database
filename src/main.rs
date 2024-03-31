5a49246da57849c76c97d70cef65cb0bcdf4e7ef
Yacin Tmimi <yacintmimi@gmail.com>
2024-03-31 19:20:44 -0400

    Store repositories from GitHub API into Database!
    This is a big step for the project. I've set things up so that I can
    qeury the GitHub API and then store those results in the databse. I've
    manually tested these changes and they're working well.
    
    As I pointed out in the code the sync calls to the GitHub API don't play
    nicely with the async database writes. At least they don't play nice if
    I were to annotate main with `#[tokio::main]` since the
    `reqwest::blocking::Client` panics when it's used in an async context. I
    have some ideas to work around this like no longer implementing Iterator
    for `RepoSearchResults` and making `RepoSearchResults::get_next_page` an
    async function, but I didn't want to make those changes yet.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Context;
-use rustfmt_user_config_db::GitHubRepoSearch;
+use rustfmt_user_config_db::{store_in_db, GitHubRepoSearch, Repository};
+use sqlx::postgres::PgPoolOptions;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
@@ -14,6 +15,9 @@ fn main() -> anyhow::Result<()> {
     let github_api_token = std::env::var("GITHUB_API_TOKEN")
         .context("Must set GITHUB_API_TOKEN environment variable")?;
 
+    let databse_url =
+        std::env::var("DATABASE_URL").context("Must set DATABASE_URL environment variable")?;
+
     let mut github_search = GitHubRepoSearch::new(&github_api_token);
     github_search
         .repositories_per_page(10)
@@ -22,16 +26,28 @@ fn main() -> anyhow::Result<()> {
 
     let mut search_results = github_search.search().unwrap();
 
-    for repository in &mut search_results {
-        println!(
-            "GraphQL ID: {}\nName: {}\nLatest Commit: {}\nPushed At: {}\n% Written in Rust {:.2}%\n",
-            repository.id(),
-            repository.name_with_owner(),
-            repository.commit_hash(),
-            repository.pushed_at(),
-            repository.percent_of_code_in_rust(),
-        );
+    // FIXME(ytmim) Need to create the async runtime manually because it doesn't play well with the
+    // syncronous `RepoSearchResults`. To get around this it probably makes sense to drop the
+    // Iterator impl for `RepoSearchResults` and instead make `get_next_page` async.
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?;
+    while let Some(repositories) = search_results.get_next_page() {
+        runtime.block_on(run_store_in_db(&databse_url, repositories))?;
     }
     println!("Next Token: {:?}", search_results.next_page());
     Ok(())
+}
+
+async fn run_store_in_db(
+    connection_str: &str,
+    repositories: Vec<Repository>,
+) -> anyhow::Result<()> {
+    let db = PgPoolOptions::new()
+        .max_connections(20)
+        .connect(connection_str)
+        .await
+        .context("can't connect to database")?;
+
+    store_in_db(db, repositories.into_iter()).await
 }
