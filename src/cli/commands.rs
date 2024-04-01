1752b8e9f23e40cdec61448ff0ee6b2071e47448
Yacin Tmimi <yacintmimi@gmail.com>
2024-04-01 02:40:01 -0400

    Add `--repo` and `--dry-run` CLI options
    These let the user search for repositories by their name and perform a
    dry-run where repositories are printed out to the console instead of
    written to the database.

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -1,5 +1,3 @@
-use std::{convert::Infallible, str::FromStr};
-
 use clap::{Parser, Subcommand};
 
 /// Manage the rustfmt user configuration database
@@ -24,5 +22,12 @@ pub enum Commands {
         /// Filter for repositories that have this number of stars or more
         #[arg(short, long, default_value_t = 50)]
         stars: u16,
+        /// The name of repository name to add. Either provide a name like `rustfmt`
+        /// or provide the repositroy name with the owner like `rust-lang/rustfmt`
+        #[arg(short, long)]
+        repo: Option<String>,
+        /// Print the git repository details instead of storing it in the database
+        #[arg(short, long, default_value_t = false)]
+        dry_run: bool,
     },
 }
