65a05e02f1a7af0fe08725d58de77e9b95950e1b
Yacin Tmimi <yacintmimi@gmail.com>
2024-04-02 13:21:13 -0400

    refactor find_rustfmt_configs to remove implicit lifetime
    The borrow checker told me that I needed to add a `+ '_` to the end of
    my `impl Iterator<Item = RustfmtConfig>` return type. By making these
    changes that's no longer necessary.

--- src/rustfmt_toml.rs
+++ src/rustfmt_toml.rs
@@ -27,30 +27,39 @@ impl std::fmt::Debug for RustfmtConfig {
 }
 
 impl<'u> crate::git::ClonedRepo<'u> {
-    pub fn find_rustfmt_configs(&self) -> impl Iterator<Item = RustfmtConfig> + '_ {
+    pub fn find_rustfmt_configs(&self) -> impl Iterator<Item = RustfmtConfig> {
         let directory_path = self.path();
         let config_files = search_for_rustfmt_config_files(directory_path);
-        config_files.into_iter().filter_map(|f| {
-            let absolute_file_path = PathBuf::from(&f);
-            let relative_path = absolute_file_path
+        let mut result = vec![];
+
+        for config in config_files {
+            let absolute_file_path = PathBuf::from(&config);
+            let Ok(relative_path) = absolute_file_path
                 .strip_prefix(self.path())
-                .map_err(|_| tracing::error!("could not find relative path"))
-                .ok()?
-                .display()
-                .to_string();
+                .map(|p| p.display().to_string())
+            else {
+                tracing::error!("could not find relative path");
+                continue;
+            };
 
-            let toml = std::fs::read_to_string(&absolute_file_path)
-                .map_err(|_| tracing::error!("unable to read toml file {relative_path}"))
-                .ok()?
-                .parse::<toml::Value>()
-                .map_err(|_| tracing::error!("unable to parse {relative_path} as toml"))
-                .ok()?;
+            let Ok(buffer) = std::fs::read_to_string(&absolute_file_path) else {
+                tracing::error!("unable to read toml file {relative_path}");
+                continue;
+            };
 
-            Some(RustfmtConfig {
+            let Ok(toml) = buffer.parse::<toml::Value>() else {
+                tracing::error!("unable to parse {relative_path} as toml");
+                continue;
+            };
+
+            let rustfmt_config = RustfmtConfig {
                 file_path: relative_path,
                 toml,
-            })
-        })
+            };
+
+            result.push(rustfmt_config)
+        }
+        result.into_iter()
     }
 }
 
